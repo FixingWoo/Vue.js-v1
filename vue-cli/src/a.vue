d7632cee49dad0c7754881a5e268e19bbcf70904
Jeong Woo <fixingwoo@gmail.com>
2023-03-02 10:22:36 +0900

    feat: vue-cli

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- HTML -->
+</template>
+<script>
+// JS - 인스턴스 옵션
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeUnmount() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+<style>
+/* CSS */
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>{{ propsdata }}</header>
+    <button type="button" v-on:click="sendEvent">send</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ["propsdata"],
+  methods: {
+    sendEvent: function () {
+      this.$emit("renew");
+    },
+  },
+};
+</script>
